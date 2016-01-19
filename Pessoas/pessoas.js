cef9969f82c711db584aced59027c76bd4f3174c
Janderson <jba1719@gmail.com>
2016-01-19 03:25:27 -0200

    first Commit

---
+++ Pessoas/pessoas.js
@@ -0,0 +1,20 @@
+var pessoasApp = angular
+  .module('pessoas' , [])
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/' , {
+        templateUrl: 'listar.html'
+      });
+  })
+  .controller('CtrlPessoas', function($scope){
+    $scope.pessoas = [
+      {nome:"Maria" , cidade:"São Paulo"},
+      {nome:"João" , cidade:"Pirapora"},
+      {nome:"Joana" , cidade:"Salvador"},
+      {nome:"Marcio" , cidade:"Londrina"},
+      {nome:"Ana" , cidade:"São Paulo"},
+      {nome:"Marta" , cidade:"Patos De Minas"},
+      {nome:"Sergio" , cidade:"Curvelo"}
+    ];
+
+  });
